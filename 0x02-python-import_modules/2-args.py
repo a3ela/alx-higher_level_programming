35976c9844e141a86f2f7bbdcb72f5b35abb40d3
a3ela <abelsintayehu626@gmail.com>
2024-04-07 09:35:43 +0300

    fixed with the prints args and just arg

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -9,6 +9,8 @@ if num_args == 1:
     print('0 arguments.')
 else:
     num_args -= 1
-    print('{} arguments:'.format(num_args))
+    print('{} argument{}:'.format(
+        num_args,
+        '' if num_args == 1 else 's'))
     for i in range(1, num_args + 1):
         print('{}: {}'.format(i, args[i]))
