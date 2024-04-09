0db4043e179f50c1a6f6fbc4eff0ece37d3965dc
a3ela <abelsintayehu626@gmail.com>
2024-04-09 13:45:21 +0300

    changed to if else

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,27 +1,22 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import sys
     from calculator_1 import add, sub, mul, div
-
-if len(sys.argv) != 4:
-    print('Usage: ./100-my_calculator.py <a> <operator> <b>')
-    exit(1)
-
-a = int(sys.argv[1])
-opr = sys.argv[2]
-b = int(sys.argv[3])
-
-match opr:
-    case '+':
-        result = add(a, b)
-    case '-':
-        result = sub(a, b)
-    case '*':
-        result = mul(a, b)
-    case '/':
-        result = div(a, b)
-    case _:
-        print('Unknown operator. Available operators: +, -, * and /')
+    import sys
+    if len(sys.argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-
-print('{} + {} = {}'.format(a , b, result))
+    operator = sys.argv[2]
+    if operator not in '*+-/':
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+    a = sys.argv[1]
+    b = sys.argv[3]
+    if operator == '+':
+        result = add(int(a), int(b))
+    elif operator == '-':
+        result = sub(int(a), int(b))
+    elif operator == '*':
+        result = mul(int(a), int(b))
+    else:
+        result = div(int(a), int(b))
+    print("{} {} {} = {}".format(a, operator, b, result))
