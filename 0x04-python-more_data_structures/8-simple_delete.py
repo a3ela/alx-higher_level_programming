2e4382c3537bbf7f44740c8536365a303e450c65
a3ela <abelsintayehu626@gmail.com>
2024-04-20 07:40:17 +0300

    removed try and except

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
     if a_dictionary:
-        try:
             del a_dictionary[key]
-        except KeyError:
-            pass
-        return a_dictionary
+    return a_dictionary
