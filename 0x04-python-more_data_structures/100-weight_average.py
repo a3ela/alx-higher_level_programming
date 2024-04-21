21e96f47aee3d027315ef9ac652cc3fc6bb349f1
a3ela <abelsintayehu626@gmail.com>
2024-04-21 10:23:00 +0300

    find avarage

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if len(my_list) == 0:
+        return 0
+
+    leftTotal = 0
+    rightTotal = 0
+    
+    for tup in my_list:
+        leftTotal += tup[0] * tup[1]
+        rightTotal += tup[-1]
+    return leftTotal/rightTotal
