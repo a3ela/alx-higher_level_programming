35599d57df63091a602c8e2774883a06731e5888
a3ela <abelsintayehu626@gmail.com>
2024-04-24 09:14:17 +0300

    fixed indent

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -6,13 +6,14 @@ class Square:
     """represent a square"""
 
     def __init__(self, size=0):
-    """ Initialize a new square
+
+        """Initialize a new square
     Args:
         size(int): the size of the square
     """
 
-    if not isinstance(size, int):
-        raise TypeError('size must be an integer')
-    if size < 0:
-        raise ValueError('size must be >= 0')
-    self.__size = size
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = size
