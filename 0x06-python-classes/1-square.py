7d53f51c791e8728462388164e62ab0f879ba2df
a3ela <abelsintayehu626@gmail.com>
2024-04-24 08:47:06 +0300

    added comment

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""define a class"""
+
+
+class Square:
+    """initialize of a new square
+    Args:
+        size(int): the size of the new square
+    """
+    def __init__(self, size):
+        self.__size = size
