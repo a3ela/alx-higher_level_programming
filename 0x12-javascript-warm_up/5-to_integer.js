b4d4572989321ae2c3dc18be1fab1c72cc0c1a68
a3bela <abelsintayehu626@gmail.com>
2024-06-19 20:31:27 +0300

    changed number to and int

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
 
-const val = process.argv[2];
+const arg = parseInt(process.argv[2]);
 
-if (val === undefined) console.log('Not a number');
-else if (isNaN(val))console.log('Not a number');
-else console.log('My number: ', Number(val));
+if (isNaN(arg)) console.log('Not a number');
+else console.log(`My number: ${arg}`);
