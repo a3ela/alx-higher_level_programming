10261077f51b25c3f888a1b9a0dd13cd055b2f7d
a3bela <abelsintayehu626@gmail.com>
2024-06-19 23:00:19 +0300

    fixed

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
 
-const str = 'C is fun';
-const arg = process.argv[2];
-
-if (isNaN(arg)) console.log('Missing number of occurences');
-else for (let i = 0; i < arg; i++){
-  console.log(str);
+const num = parseInt(process.argv[2], 10);
+if (isNaN(num)) {
+  console.log('Missing number of occurences');
+} else {
+  for (let i = num; i > 0; i -= 1) {
+    console.log('C is fun');
+  }
 }
