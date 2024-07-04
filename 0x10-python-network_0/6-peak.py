6ac89f7bbfd41b666ee009debae1e54abd168e8b
a3bela <abelsintayehu626@gmail.com>
2024-07-05 01:05:42 +0300

    done

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,35 +1,31 @@
 #!/usr/bin/python3
+"""
+Function that finds a peak in a list of unsorted integers
+"""
+
+
 def find_peak(list_of_integers):
-  """
-  Finds a peak (an element greater than its neighbors) in a list of unsorted integers.
+    """
+    finds num that's greater than both left and right
+    """
+    if len(list_of_integers) == 0:
+        return None
 
-  Args:
-      list_of_integers: A list of integers.
+    l = list_of_integers
+    beg = 0
+    end = len(l)-1
 
-  Returns:
-      The first peak element found in the list, or None if no peak is found.
-  """
-  
-  # Handle edge cases: empty list or list with one element
-  if len(list_of_integers) <= 1:
-    return None
+    if l[beg] > l[beg+1]:
+        return l[beg]
+    if l[end] > l[end-1]:
+        return l[end]
 
-  # Recursive binary search to find a peak
-  def binary_search(start, end):
-    if start == end:
-      return list_of_integers[start]  # Single element is considered a peak
-
-    mid = (start + end) // 2
-
-    # Check if middle element is a peak
-    if (mid > 0 and list_of_integers[mid] > list_of_integers[mid - 1]) and (mid < len(list_of_integers) - 1 and list_of_integers[mid] > list_of_integers[mid + 1]):
-      return list_of_integers[mid]
-
-    # If middle element is not a peak, search for peak in the appropriate half
-    elif mid > 0 and list_of_integers[mid] < list_of_integers[mid - 1]:
-      return binary_search(start, mid - 1)
+    mid = (beg+end)//2
+    if l[mid-1] < l[mid] and l[mid+1] < l[mid]:
+        return l[mid]
+    if l[mid] < l[mid-1]:
+        return find_peak(l[beg:mid+1])
+    elif l[mid] < l[mid+1]:
+        return find_peak(l[mid:end+1])
     else:
-      return binary_search(mid + 1, end)
-
-  # Initiate the binary search on the entire list
-  return binary_search(0, len(list_of_integers) - 1)
+        return l[beg]
