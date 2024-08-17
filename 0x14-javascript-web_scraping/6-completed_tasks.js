c88ea39063a74f78c9ca2627f04fe4309d479784
a3ela <abelsintayehu626@gmail.com>
2024-08-18 00:32:56 +0300

    fixed semistandard and refactored the function

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,26 +1,25 @@
 #!/usr/bin/node
 // script that computes the number of tasks completed by user id.
 
-const args = process.argv;
-let reqURL = args[2];
-let request = require('request');
-request(reqURL, function (error, response, body) {
-  if (error) {
-    console.log('error:', error);
+const url = process.argv[2];
+const request = require('request');
+
+request(url, function (err, response, body) {
+  if (err) {
+    console.log(err);
   } else {
-    let todos = JSON.parse(body);
-    let dash = {};
-    for (let i = 0; i < todos.length; i++) {
-      let status = (todos[i]['completed']);
-      let key = todos[i]['userId'].toString();
-      if (status) {
-        if (dash[key]) {
-          dash[key]++;
-        } else {
-          dash[key] = 1;
+    const data = JSON.parse(body);
+    const completedTasks = {};
+
+    data.forEach(task => {
+      if (task.completed) {
+        if (!completedTasks[task.userId]) {
+          completedTasks[task.userId] = 0;
         }
+        completedTasks[task.userId]++;
       }
-    }
-    console.log(dash);
+    });
+
+    console.log(completedTasks);
   }
 });
